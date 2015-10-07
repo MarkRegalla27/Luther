11acda2db1bbe0cd5a375776c2085afaec7b252e
Mark Regalla <regalla.mark@gmail.com>
2015-10-06 23:00:37 -0400

    made progress toward getting director data

--- DataReading.py
+++ DataReading.py
@@ -4,6 +4,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import dateutil.parser
 import pickle
+from datetime import datetime
 
 url2015List = ['http://www.boxofficemojo.com/yearly/chart/?page=1&view=releasedate&view2=domestic&yr=2015&p=.htm',
 'http://www.boxofficemojo.com/yearly/chart/?page=2&view=releasedate&view2=domestic&yr=2015&p=.htm',
@@ -36,14 +37,40 @@ url2012List = ['http://www.boxofficemojo.com/yearly/chart/?page=1&view=releaseda
                 'http://www.boxofficemojo.com/yearly/chart/?page=6&view=releasedate&view2=domestic&yr=2012&p=.htm',
                 'http://www.boxofficemojo.com/yearly/chart/?page=7&view=releasedate&view2=domestic&yr=2012&p=.htm']
 
+yearList = [2015, 2014, 2013, 2012]
 urlList = [url2015List, url2014List, url2013List, url2012List]
-indexColumns = ['Rank', 'Title', 'Studio', 'Total Gross', 'Total Theaters', 'Opening Gross',
-               'Opening Theathers', 'Opening Date', 'Closing Date']
+rowforindex = ['Rank', 'Title', 'Studio', 'Total Gross', 'Total Theaters', 'Opening Gross',
+               'Opening Theaters', 'Opening Date', 'Closing Date', 'Year']
+totalFrame = pd.DataFrame()
 
+j = 0
 for i in urlList:
     for k in i:
-        holderFrame = pd.read_html(i)
+        holderFrame = pd.read_html(k)
         holderFrame = holderFrame[5]
         holderFrame = holderFrame.drop(holderFrame.index[0:2])
         holderFrame = holderFrame.drop(holderFrame.index[-4:])
-        #ensure column labels are dropped on all dataframes that get appended
+        holderFrame['year'] = yearList[j]
+        totalFrame = totalFrame.append(holderFrame, ignore_index=True)
+    j += 1
+
+totalFrame.columns = rowforindex
+totalFrame = totalFrame.dropna()
+
+#Strip $ signs from money amounts and convert them to floats
+totalFrame['Opening Gross'] = totalFrame['Opening Gross'].map(lambda x: x.strip('$'))
+totalFrame['Total Gross'] = totalFrame['Total Gross'].map(lambda x: x.strip('$'))
+totalFrame['Opening Gross'] = totalFrame['Opening Gross'].map(lambda x: x.replace(',',''))
+totalFrame['Total Gross'] = totalFrame['Total Gross'].map(lambda x: x.replace(',',''))
+totalFrame['Opening Gross'] = totalFrame['Opening Gross'].map(lambda x: float(x))
+totalFrame['Total Gross'] = totalFrame['Total Gross'].map(lambda x: float(x))
+
+#Convert number of theaters to floats
+totalFrame['Total Theaters'] = totalFrame['Total Theaters'].map(lambda x: float(x))
+totalFrame['Opening Theaters'] = totalFrame['Opening Theaters'].map(lambda x: float(x))
+
+#Attempt to convert date from unicode to datetime.  Not working as of 10-1-15
+#df['Opening Date'] = df['Opening Date'].map(lambda x: datetime.strptime(x,'%m/%d'))
+
+with open('totalFrame.pkl', 'w') as picklefile:
+    pickle.dump(totalFrame, picklefile)
